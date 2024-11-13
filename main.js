ca037814ae5e7b3061c9d62b2a2fab00c8b94e16
Juno <138246438+LeeT27@users.noreply.github.com>
2024-11-13 09:14:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -20,18 +20,55 @@ const config = {
 
 let player;
 let isJumping = false;
+let ground;
+let speed = 3; // How fast the ground moves to the left
+let groundWidth;
+let block;
 
 const game = new Phaser.Game(config);
 function preload() {
   this.load.image('player', 'assets/player.png');
+  this.load.image('ground', 'assets/ground.png');
+  this.load.image('block', 'assets/block.webp');
 }
 
 function create() {
-  player = this.physics.add.image(128, 500, 'player');
+  player = this.physics.add.image(128, 650, 'player');
   player.setDisplaySize(64, 64);
   this.input.on('pointerdown', jump, this);
+
+  groundWidth = 125;
+  ground = this.physics.add.group();
+  for (let i = 0; i < 10; i++) {
+    let tile = this.add.tileSprite(i * 125, 720, 148, 125, 'ground');
+    ground.add(tile);
+  }
+  this.physics.add.collider(
+    player,
+    ground,
+    handlePlayerGroundCollision,
+    null,
+    this
+  );
+
+  block = this.physics.add.image(800, 560, 'block');
+  block.setDisplaySize(64, 64);
+
+  this.physics.add.collider(
+    player,
+    block,
+    handlePlayerBlockCollision,
+    null,
+    this
+  );
 }
 
+function handlePlayerGroundCollision(player, tile) {
+  player.y = 625;
+}
+function handlePlayerBlockCollision(player, block) {
+  player.y = block.y - 64;
+}
 function jump() {
   if (isJumping) return;
   // Prevent starting multiple jumps at once
@@ -64,4 +101,14 @@ function jump() {
   });
 }
 
-function update() {}
+function update() {
+  block.x -= speed;
+  ground.getChildren().forEach((tile) => {
+    tile.x -= speed;
+
+    // If a tile moves off-screen to the left, reset its position to the right
+    if (tile.x + groundWidth < 0) {
+      tile.x = ground.getChildren().length * groundWidth - groundWidth;
+    }
+  });
+}
